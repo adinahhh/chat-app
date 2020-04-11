7c65ee143b750467a3fcbef959c091b701dcc5da
vagrant <vagrant@vagrant.vm>
2020-04-11 22:24:37 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secretkey"
+socketio = SocketIO(app)
+
+# using a decorator here, listening for message event
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: ' + msg)
+
+    # send out msg to everyone on server
+    # broadcast defaults to send msg to client you received a msg from
+    # can break this into namespaces, and rooms; check into this later
+    send(msg, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
